4bb888bf8bca37f448c9e8bf346fa5c2a64c43fe
bruce <asd1979009ab@gmail.com>
2018-05-18 10:33:18 +0800

    git-demo

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+  int i ,sum;
+  for(i=1;i<11;i++)
+  {
+  	sum=sum+i;
+  }	
+	  printf("1+¨ì10= %d",sum);
+	return 0;
+}
